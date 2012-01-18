ec422d7dd78a8c9d234cd3921d7d86138c3909a3
Alexandru Scvortov <scvalex@gmail.com>
2012-01-18 16:08:35 +0000

    full support for Java in Makefile

--- problem.mk
+++ problem.mk
@@ -1,17 +1,20 @@
 TESTS ?=
 OBJECTS := $(patsubst %.cpp, %.o, $(wildcard *.cpp))
 
-TARGET := $(shell basename $$(pwd))
+PROBLEM := $(shell basename $$(pwd))
 
 JAVA_SOURCES = $(wildcard *.java)
 ifneq "$(JAVA_SOURCES)" ""
-TARGET := $(TARGET).class
+TARGET := $(PROBLEM).class
+RUN_COMMAND := java $(TARGET)
 endif
 
 CXX_SOURCES = $(wildcard *.cpp)
 ifneq "$(CXX_SOURCES)" ""
+TARGET := $(PROBLEM)
+RUN_COMMAND := ./$(TARGET)
 $(TARGET): $(OBJECTS)
-	g++ -Wall -O2 -lm $(TARGET).cpp -o $(TARGET)
+	g++ -Wall -O2 -lm $(OBJECTS) -o $(TARGET)
 endif
 
 all: $(TARGET)
@@ -19,15 +22,15 @@ all: $(TARGET)
 .PHONY: all build test
 
 .cpp.o:
-	g++ -Wall $< -o $@
+	g++ -c -Wall $< -o $@
 
 .SUFFIXES: .java .class
 .java.class:
 	javac $<
 
-test: build
+test: $(TARGET)
 	mkdir -p test
-	$(foreach i,$(TESTS),./$(TARGET) < $(TARGET).$(i).in > test/$(TARGET).$(i).out && diff $(TARGET).$(i).out test/$(TARGET).$(i).out)
+	$(foreach i,$(TESTS),$(RUN_COMMAND) < $(PROBLEM).$(i).in > test/$(PROBLEM).$(i).out && diff $(PROBLEM).$(i).out test/$(PROBLEM).$(i).out)
 
 clean:
 	rm -rf test/ *.class *.o $(TARGET)
