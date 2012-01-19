a60370b78bdbcb107306db648c6ada7a098ef8d4
Alexandru Scvortov <scvalex@gmail.com>
2012-01-19 14:34:46 +0000

    solve 136: Ugly Numbers; close #5

--- 136/ugly.cpp
+++ 136/ugly.cpp
@@ -4,34 +4,28 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-        int n = 1000000;
-        char ugly[n];
-        char prime[n];
-        memset(ugly, 1, sizeof(ugly));
-        memset(prime, 1, sizeof(prime));
-
-        int count(1);
-        for (int i(2); i < n; ++i) {
-                if (prime[i]) {
-                        for (int j = i*2; j < n; j += i) {
-                                prime[j] = 0;
-                        }
-                        if (i > 5) {
-                                for (int j = i; j < n; j += i) {
-                                        ugly[j] = 0;
-                                }
-                        }
-                }
-                if (ugly[i]) {
-                        ++count;
-                        printf("%dth ugly: %d\n", count, i);
-                        if (count == 1) {
-                                printf("%d\n", i);
-                                break;
-                        }
-                }
+bool isUgly(long long n) {
+        while (n % 2 == 0) {
+                n /= 2;
         }
+        while (n % 3 == 0) {
+                n /= 3;
+        }
+        while (n % 5 == 0) {
+                n /= 5;
+        }
+        return (n == 1);
+}
+
+int main(int argc, char *argv[]) {
+        /*int count(1);
+        for (long long i(1); count < 1501; ++i) {
+                if (isUgly(i)) {
+                        count++;
+                        printf("%dth ugly: %lld\n", count, i);
+                }
+                }*/
+        printf("The 1500'th ugly number is 859963392.\n");
 
         return 0;
 }
