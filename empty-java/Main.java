ffe37398191bde20342646b4e23fc1b77165bec9
Paul Grigoras <paul.grigoras90@gmail.com>
2012-01-19 14:01:30 +0000

    Added task to create problem skeleton (Java).

---
+++ empty-java/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+	}
+}
