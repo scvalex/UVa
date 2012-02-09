b5e45227be66b8c987087e61e508bce96b77c21c
Paul Grigoras <paul.grigoras90@gmail.com>
2012-02-09 22:31:58 +0000

    Skeleton for 10616.

---
+++ 10616/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+
+		long [][][] sol;
+		
+		int [] numbers;
+		int nTests = 1;
+		
+		boolean first = true;		
+		
+		int n,q,d,m;
+		
+		while (sc.hasNext()) {
+			n = sc.nextInt();
+			q = sc.nextInt();
+			
+			if ( n == 0 && q == 0 )
+				break;
+			
+			numbers = new int[n + 1];
+			
+			for (int i=1;i<=n;i++) {
+				numbers[i] = sc.nextInt();
+			}
+			first = true;
+			System.out.println("SET " + (nTests++) + ":");
+			for (int qq=1;qq<=q;qq++) {
+				d = sc.nextInt();
+				m = sc.nextInt();
+			
+				sol = new long[n + 1][m + 1][d + 1];
+				
+				for (int i=1;i<=n;i++) {
+						for (int k=0;k<d;k++) {
+								sol[i][1][k] = sol[i-1][1][k];
+						}
+						sol[i][1][mod(numbers[i],d)]++;
+				}
+				
+				int r;
+				for (int i=1;i<=n;i++) {
+					r = mod (numbers[i], d);
+					for (int j=2; j<=m; j++){
+						for (int k=0; k<d; k++) {
+							sol[i][j][ (k + r) % d] = sol[i-1][j][(k + r) % d] + sol[i-1][j-1][k];
+						}
+					}
+				}
+				
+				System.out.println("QUERY " + qq + ": " + sol[n][m][0]);
+			}
+		}
+		
+		sc.close();
+	}
+
+	public static int mod(int a, int b) {
+		int c = a % b;
+		if ( a < 0 )
+			c += a;
+		return c;
+	}
+	
+}
---
+++ 10616/README.md
@@ -0,0 +1,5 @@
+Divisible Group Sums
+=======
+
+> [Problem statement](http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1557)
+
