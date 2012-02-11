44d76d69e149a0bace3ce97a1e67e232a8b43096
paul-g <paul.grigoras90@gmail.com>
2012-02-11 16:29:17 +0200

    Solved 374

---
+++ 374/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int b,p,m;
+		
+		modPow(3,0,1);
+		
+		while (sc.hasNext()) {
+			b = sc.nextInt();
+			p = sc.nextInt();
+			m = sc.nextInt();
+
+			if ( m == 1 )
+				System.out.println("0");
+			else if ( p == 0 ) 
+				System.out.println("1");
+			else 
+				System.out.println(modPow(b,p,m));	
+		}
+		
+		sc.close();
+	}
+	
+	static int modPow(int b,int p,int m) {
+		if ( p == 1 )
+			return b % m;
+		int r  = modPow(b, p/2, m) % m;
+		if ( p % 2 == 0) {
+			return (r * r) % m;
+		} else {
+			return (((r * r) % m ) * (b % m)) % m;	
+		}
+			
+	}
+}
