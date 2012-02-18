155403f1b2c37a9cc2020fe11312c1b051c23806
paul-g <paul.grigoras90@gmail.com>
2012-02-18 02:48:36 +0200

    Solved C

--- fau-winter-2012/C/Main.java
+++ fau-winter-2012/C/Main.java
@@ -3,21 +3,72 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+
+	static final int MAX_SIZE = 100001;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 		
 		int nTests = sc.nextInt();
 		
-		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-		int s,q,l,u,i,j;
-		while (sc.hasNext()) {
+		PriorityQueue<Material> queue = new PriorityQueue<Material>();
+		ArrayList<Integer> queries = new ArrayList<Integer>();
+		ArrayList<Material> materials = new ArrayList<Material>();
+		
+		int s,q,l,u,i, query;
+		long j;
+		long L, U, I;
+		long max;
+		int kk = 0;
+		boolean first = true;
+		while ( kk < nTests) {
+			kk++;
+
+			queue.clear();
+			materials.clear();
+			queries.clear();
+
 			s = sc.nextInt();
 			q = sc.nextInt();
-			l = sc.nextInt();
-			u = sc.nextInt();
-			i = sc.nextInt();
-			for (j = 0;j<=u; j+=i) {
-				queue.add(j);
+			
+			for (i=0;i<s;i++) {
+				Material m = new Material(sc.nextLong(), sc.nextLong(), sc.nextLong(), (i+1));
+				queue.add(m);
+			}
+
+			//System.out.println("------------------------");
+
+			max = 0;
+			for (i=0;i<q;i++) {
+				query = sc.nextInt();
+				queries.add(query);
+				max = Math.max(query, max);
+			}
+			
+			while (true) {
+				//System.out.println(queue.toString());
+				//System.out.println(materials.toString());
+				
+				Material top = queue.poll();
+				if ( top == null )
+					break;	
+				
+				materials.add(new Material(top));
+				
+				if ( materials.size() > max + 1 || materials.size() > MAX_SIZE )
+					break;
+				
+				if ( top.canReuse() ) {
+					queue.add(top.inc());
+				}
+			}
+
+			//System.out.println(s + " " + q);
+			//System.out.println(materials.toString());
+			
+			for (i=0;i<q;i++) {
+				Material m = materials.get(queries.get(i) - 1);
+				System.out.println(m.currentHeight + " " + m.type);
 			}
 			
 		}
@@ -26,12 +77,40 @@ public class Main {
 	}
 }
 
-class Pair implements Comparable<Pair>{
-	int height;
+class Material implements Comparable<Material>{
+	long currentHeight;
+	long maxHeight;
+	long inc;
 	int type;
+	Material(Material m) {
+		this.currentHeight = m.currentHeight;
+		this.maxHeight = m.maxHeight;
+		this.inc = m.inc;
+		this.type = m.type;
+	}
+	Material(long l, long u, long i, int type) {
+		this.currentHeight = l;
+		this.maxHeight = u;
+		this.inc = i;
+		this.type = type;
+	}
 	
 	@Override
-	public int compareTo(Pair p) {
-		return this.height - p.height;
+	public int compareTo(Material m) {
+		if (currentHeight == m.currentHeight)
+			return type - m.type;
+		if ( currentHeight < m.currentHeight)
+			return -1;
+		return 1;
+	}
+	boolean canReuse(){
+		return currentHeight + inc <= maxHeight;
+	}
+	Material inc() {	
+		currentHeight += inc;
+		return this;
+	}
+	public String toString() {
+		return "(" + currentHeight +  ", " + maxHeight + ", " +  inc + ", " + type + ")";
 	}
 }
