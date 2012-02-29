1128210c9a2abfdd309ef4d93812fa622d3a6c89
paul-g <paul.grigoras90@gmail.com>
2012-02-29 02:51:43 +0200

    BigInteger solution for 10220.

---
+++ uva-online/10220/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+        
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+	while (sc.hasNext()) {
+	    int n = sc.nextInt();
+	    BigInteger prod = new BigInteger("1");
+	    for (int i=1; i<=n; i++) {
+		prod = prod.multiply(new BigInteger(Integer.toString(i)));
+	    }
+
+	    int sumDigits = 0;
+	    for (char c : prod.toString().toCharArray()) {
+		sumDigits += (c - 48);
+	    }
+
+	    System.out.println(sumDigits);
+	}
+	sc.close();
+    }
+}
---
+++ uva-online/10220/README.md
@@ -0,0 +1,5 @@
+10220 - I Love Big Numbers !
+=======
+
+> [Problem statement](http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1161)
+
