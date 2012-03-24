ed8f931a73dd4a4f57dc349cdb15acbf41410050
paul-g <paul.grigoras90@gmail.com>
2012-03-24 22:34:54 +0000

    458 in java...

---
+++ uva-online/458j/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+	Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+	DataInputStream din = new DataInputStream(System.in);
+
+	DataOutputStream dout = new DataOutputStream(System.out);
+
+	boolean first = true;
+	
+	while (true) {
+	    try {
+		byte b = din.readByte();
+		if (b != 10)
+		    dout.write(b - 7);
+		else 
+		    System.out.println();
+	    } catch (EOFException e) {
+		break;
+	    }
+
+	}
+
+	sc.close();
+    }
+}
---
+++ uva-online/458j/README.md
@@ -0,0 +1,5 @@
+458 - The Decoder
+=======
+
+> [Problem statement](http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=399)
+
