8c87676a62d8cd7ac0978b45df28b1d21569a923
paul-g <paul.grigoras90@gmail.com>
2012-03-29 21:48:20 +0100

    Fixed formatting

--- uva-online/10034/Main.java
+++ uva-online/10034/Main.java
@@ -4,59 +4,59 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-	Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
-	Point fs[];
-	double d[][];
-	int nPoints;
+        Point fs[];
+        double d[][];
+        int nPoints;
 
-	Set<Integer> V = new HashSet<Integer>();
+        Set<Integer> V = new HashSet<Integer>();
 
-	int n = sc.nextInt();
-	for (;n>0;n--) {
-	    nPoints = sc.nextInt();
-	    fs = new Point[nPoints + 1];
-	    for (int i=0;i<nPoints;i++) {
-		fs[i] = new Point(sc.nextDouble(), sc.nextDouble());
-	    }
+        int n = sc.nextInt();
+        for (;n>0;n--) {
+            nPoints = sc.nextInt();
+            fs = new Point[nPoints + 1];
+            for (int i=0;i<nPoints;i++) {
+                fs[i] = new Point(sc.nextDouble(), sc.nextDouble());
+            }
 
-	    d = new double[nPoints][nPoints];
+            d = new double[nPoints][nPoints];
 
-	    for (int i=0;i<nPoints;i++)
-		for (int j=0;j<nPoints;j++)
-		    d[i][j] = Math.sqrt(Math.pow(fs[i].x - fs[j].x, 2) + Math.pow(fs[i].y - fs[j].y ,2));
-	
-	    V.add(1);
-	    double res = 0; 
+            for (int i=0;i<nPoints;i++)
+                for (int j=0;j<nPoints;j++)
+                    d[i][j] = Math.sqrt(Math.pow(fs[i].x - fs[j].x, 2) + Math.pow(fs[i].y - fs[j].y ,2));
+        
+            V.add(1);
+            double res = 0; 
 
-	    while (V.size() < nPoints) {
+            while (V.size() < nPoints) {
 
-		double min = Double.MAX_VALUE;
+                double min = Double.MAX_VALUE;
 
-		Edge e = null;
+                Edge e = null;
 
-		for (Integer v : V) {
-		    for (int i=0;i<nPoints;i++) {
-			if (!V.contains(i) && d[v][i] < min) {
-			    min = d[v][i];
-			    e = new Edge(v ,i, d[v][i]);
-			}
-		    }
-		}
-		
-		V.add(e.y);
-		res += e.weight;
-	    }
+                for (Integer v : V) {
+                    for (int i=0;i<nPoints;i++) {
+                        if (!V.contains(i) && d[v][i] < min) {
+                            min = d[v][i];
+                            e = new Edge(v ,i, d[v][i]);
+                        }
+                    }
+                }
+                
+                V.add(e.y);
+                res += e.weight;
+            }
 
-	    V.clear();
+            V.clear();
 
-	    if ( n > 1 ) {
-		System.out.format("%.2f\n\n", res);
-	    } else {
-		System.out.format("%.2f\n", res);
-	    }
-	}
-	sc.close();
+            if ( n > 1 ) {
+                System.out.format("%.2f\n\n", res);
+            } else {
+                System.out.format("%.2f\n", res);
+            }
+        }
+        sc.close();
     }
 }
 
@@ -64,8 +64,8 @@ class Point {
     double x;
     double y;
     public Point(double x, double y) {
-	this.x = x;
-	this.y = y;
+        this.x = x;
+        this.y = y;
     }
 }
     
@@ -74,8 +74,8 @@ class Edge {
     int y;
     double weight;
     public Edge(int x, int y, double weight) {
-	this.x = x;
-	this.y = y;
-	this.weight = weight;
+        this.x = x;
+        this.y = y;
+        this.weight = weight;
     }
 }
