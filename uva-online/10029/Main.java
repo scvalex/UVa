1138ce6dbadb5803821c2186afbff078d3666324
paul-g <paul.grigoras90@gmail.com>
2012-07-07 14:21:59 +0100

    Solved 10029.

--- uva-online/10029/Main.java
+++ uva-online/10029/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 
-    static final char words[][] = new char[25000][16];
+    static final String words[] = new String[25000];
 
     public static void main(String[] args) throws IOException {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,43 +11,47 @@ public class Main {
 	int nWords = 0;
 	String line;
 	
-	while ( (line = br.readLine()) != null) {
-	    words[nWords++] = line.toCharArray();
-	}
-
-	int best = 1;
-
 	Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
-	map.put(1, new ArrayList<String>());
+	Map<String, Integer> l = new HashMap<String, Integer>();
 
-	for (int i=0;i<nWords;i++) {
-	    boolean found = false;
-	    String crtWord = new String(words[i]);
-
-	    for (int ll=best; ll>0 && !found; ll--) {
-		if (map.get(ll) != null) {
-		    for (String s : map.get(ll)) {
-			if ( editStep(words[i], s.toCharArray()) ) {
-			    List<String> list = map.get(ll + 1);
-			    if (list == null) {
-				map.put(ll + 1, list = new ArrayList<String>());
-			    }
-			    list.add(crtWord);
-			    found = true;
-			    best = Math.max(best, ll + 1);
-			    break;
-			}
-		    }
-		}
-	    }
-
-	    if (!found) {
-		map.get(1).add(crtWord);
-	    }
+	while ( (line = br.readLine()) != null) {
+	    words[nWords++] = line;
 	}
 
-	System.out.print(best);
+	int longest = 0;
+	for (int i=0;i<nWords;i++) {
+	    int best = 0;
+	    for (Integer hash : possibleHashes(words[i])) {
+		if (map.get(hash) != null) {
+		    for (String s : map.get(hash)) {
+			if (editStep(words[i].toCharArray(), s.toCharArray()))
+			    best = Math.max(best, l.get(s) + 1);
+		    }
+		} else {
+		    map.put(hash, new ArrayList<String>());
+		}
+		map.get(hash).add(words[i]);
+	    }
+	    l.put(words[i], best);
+	    longest = Math.max(best, longest);
+	}
+
+	// gets WA without newline
+	System.out.println(longest + 1);
 	br.close();
+    }
+
+    static Collection<Integer> possibleHashes(String word) {
+	Collection<Integer> hashes = new HashSet<Integer>();
+	hashes.add(word.hashCode());
+	String toHash;
+
+	for (int i=0;i<word.length();i++) {
+	    toHash = word.substring(0, i) + word.substring(i + 1);
+	    hashes.add(toHash.hashCode());
+	}
+
+	return hashes;
     }
 
     static boolean editStep(char[] word1, char[] word2) {
@@ -62,22 +66,22 @@ public class Main {
 	    editStep = hasOneDiffChar(word1, word2);
 	    editStep |= hasOneDiffChar(word2, word1);
 	}
-
+	
 	return editStep;
     }
 
+    
     static boolean hasOneDiffChar(char[] word1, char[] word2) {
 	if (word1.length == word2.length + 1) {
 	    int diff = 0;
-	    for (int i=0;i<word2.length && diff < 2;i++)
-		if (word1[i + diff] != word2[i]) {
-		    diff++;
-		    i--;
-		}
-	    return (diff < 2);
-	}
-
-	return false;
-    }
-
+		for (int i=0;i<word2.length && diff < 2;i++)
+		    if (word1[i + diff] != word2[i]) {
+			diff++;
+			i--;
+		    }
+		return (diff < 2);
+        }
+	
+       return false;
+     }
 }
--- uva-online/10029/Test.java
+++ uva-online/10029/Test.java
@@ -1,14 +1,22 @@
+import java.util.*;
+
 public class Test {
     public static void main (String[] args) {
 	int n = 25000;
+	ArrayList<Character> chars = new ArrayList<Character>();
+	for ( Character c : "abcdefghabcdefgh".toCharArray()) {
+	    chars.add(c);
+	}
+
 	if (args.length > 0)
 	    n = Integer.parseInt(args[0]);
+
 	for (int i=0;i<25000;i++){
-	    System.out.print("abcdefghabcdefg");
-	    if (i%2==0)
-		System.out.println();
-	    else 
-		System.out.println("h");
+	    Collections.shuffle(chars);
+	    for (char c : chars) {
+		System.out.print(c);
+	    }
+	    System.out.println();
 	}
     }
 	
